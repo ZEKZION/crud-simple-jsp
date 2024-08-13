3847453cd04f02b253c12b5982dff84886662b1b
Luis Bailon <luis_abv@outlook.com>
2024-08-13 18:57:45 -0500

    cambio de parametros

--- src/main/java/db/Parametros.java
+++ src/main/java/db/Parametros.java
@@ -1,8 +1,9 @@
 package db;
 
 public interface Parametros {
-    String url = "jdbc:mysql://localhost:3306/dbsistema";
+    String url = "jdbc:mysql://localhost:3309/dbsistema";
     String user = "root";
-    String password = "78465076";
-    String driver = "com.mysql.cj.jdbc.Driver";
+    String password = "";
+    // String driver = "com.mysql.cj.jdbc.Driver"; // otra opcion para cargar el driver 
+    String driver = "com.mysql.jdbc.Driver";
 }
